e19bcb1c20f9e268754ef80ac6f8323d8d0ef875
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2026-01-22 15:31:41 +0300

    Remove redudant def

--- Target/RISC-V/operands.rb
+++ Target/RISC-V/operands.rb
@@ -3,6 +3,5 @@ module SimInfra
     def Imm20(name) = Operand.new(name, :operand, 20)
     def Imm12(name) = Operand.new(name, :operand, 12)
     def Imm5(name) = Operand.new(name, :operand, 5)
-    def Imm5(name) = Operand.new(name, :operand, 5)
     def Imm7(name) = Operand.new(name, :operand, 7)
 end
